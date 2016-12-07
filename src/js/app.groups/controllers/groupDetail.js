81e5c5414aea8d5a9697b8e332278a3853af7faf
Tai Chen <mindlikewater@gmail.com>
2016-12-07 16:24:37 -0500

    Now hiding the join group button if already a group member

--- src/js/app.groups/controllers/groupDetail.js
+++ src/js/app.groups/controllers/groupDetail.js
@@ -1,47 +1,52 @@
-function GroupDetailController (GroupService, $stateParams, $state) {
+function GroupDetailController (GroupService, $stateParams, $state, $cookies) {
 
   let vm = this;
   vm.id = $stateParams.id;
+  vm.userID = $cookies.get('userID');
   vm.group = {};
   vm.address = {};
   vm.memberList = memberList;
   vm.addMember = addMember;
   vm.addMeeting = addMeeting;
+  vm.isAMember = true;
 
   function init () {
     GroupService.groupDetail(vm.id).then((resp) => {
       vm.group = resp.data.group[0];
       // console.log(vm.group);
-
     });
-    memberList()
+    memberList();
   };
 
   init();
 
   function addMeeting (mtg) {
-    // console.log("meeting function")
     mtg.group_id = vm.id;
     GroupService.groupAddMeeting(mtg).then((resp) => {
       vm.addMeeting = resp.data;
-      console.log(vm.addMeeting)
-      $state.go('root.home')
-    });
-  };
-
-  function addMember () {
-    GroupService.groupAddMember(vm.id).then((resp) => {
-      vm.addMember = resp.data;
+      // console.log(vm.addMeeting)
+      $state.go('root.home');
     });
   };
 
   function memberList () {
     GroupService.groupMemberList(vm.id).then((resp) => {
       vm.memberList = resp.data;
+      let memBool = false;
+      vm.memberList.forEach(member => {
+        if (Number(vm.userID) === member.user_id) memBool = true;
+      });
+      if (!memBool) vm.isAMember = false;
+    });
+  };
+
+  function addMember () {
+    GroupService.groupAddMember(vm.id).then((resp) => {
+      if (vm.userID !== vm.memberList.user_id) vm.addMember = resp.data;
     });
   };
 
 };
 
-GroupDetailController.$inject = ['GroupService', '$stateParams', '$state'];
+GroupDetailController.$inject = ['GroupService', '$stateParams', '$state', '$cookies'];
 export { GroupDetailController };
