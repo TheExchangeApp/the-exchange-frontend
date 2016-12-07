57fd67740a654951f9cd3b907a6a06c936b70aa0
Grace Lee <eunheh@gmail.com>
2016-12-07 18:42:06 -0500

    test adding lat lng

--- src/js/app.groups/controllers/groups.js
+++ src/js/app.groups/controllers/groups.js
@@ -3,6 +3,9 @@ function GroupsController (GroupService, $state) {
   let vm = this;
 
   vm.groups = [];
+  vm.address = {};
+  vm.lag = {};
+  vm.lng = {};
   vm.add = add;
   vm.search = search;
   vm.detail = detail;
@@ -13,31 +16,37 @@ function GroupsController (GroupService, $state) {
   }
 
   function add (group) {
+    // console.log("the group is: ", group)
+    let address = `${group.street} ${group.zip}`
+    googleLocator(group, address);
     GroupService.groupAdd(group).then((resp) => {
-      $state.go('root.home');
+      console.log(group)
+      // $state.go('root.home');
     });
   }
 
   function search (group) {
-    console.log("the group is: ", group)
+    // console.log("the group is: ", group)
     let address = `${group.street} ${group.zip}`
-    console.log(address)
+    googleLocator(group, address);
+    // console.log(address)
     GroupService.groupSearch(group).then((resp) => {
       vm.groups = resp.data;
       vm.showResults = true;
-      googleLocator(address);
     });
   }
 
-  function googleLocator (address) {
+  function googleLocator (group, address) {
     var geocoder;
     var map;
     geocoder = new google.maps.Geocoder();
     geocoder.geocode( { 'address': address}, function(results, status) {
-      console.log('results', results[0])
+      // console.log('results', results[0])
       if (status == 'OK') {
-        console.log(results[0].geometry.location.lat());
-        console.log(results[0].geometry.location.lng());
+        group.lat = results[0].geometry.location.lat();
+        group.lng = results[0].geometry.location.lng();
+        // console.log(results[0].geometry.location.lat());
+        // console.log(results[0].geometry.location.lng());
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
       }
